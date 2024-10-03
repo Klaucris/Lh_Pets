e772a9d9659fcab6857a744dca0de99e7bbe8e95
Klaucris <159507043+Klaucris@users.noreply.github.com>
2024-10-03 03:12:12 -0300

    LH Pets

---
+++ Models/Cliente.cs
@@ -0,0 +1,50 @@
+namespace LHPet.Models;
+public class Cliente
+{
+/* No trecho de código a seguir são criados os 
+atributos de Cliente e seus getters e setters
+correspondentes. */
+public int Id { get ; set;}
+public string Nome {get; set;}
+public string Cpf {get; set;}
+public string Email {get; set; }
+public string Paciente {get; set;}
+
+    //Construtor ds classe
+   // public Cliente(int id,string nome,string cpf,string email,string paciente)
+
+   // {
+   // Id = id;
+   // Nome = nome;
+   // Cpf = cpf;
+   // Email = email;
+   // Paciente = paciente;
+   // }    
+    
+    //public Cliente(int id, string nome, string cpf,string email,string paciente)
+
+   // {
+    //Id = id;
+    //Nome = nome;
+    //Cpf = cpf;
+    //Email = email;
+    //Paciente = paciente;
+    //}//    
+    
+
+/* Nesse trecho do código, é criado o Construtor do 
+model Cliente e determinado quais parâmetros são 
+necessários na criação das instâncias. */
+
+public Cliente(int id ,string nome , string cpf , string email, string paciente)
+{
+    this.Id = id ;
+    this.Nome = nome ;
+    this.Cpf = cpf;
+    this.Email = email;
+    this.Paciente = paciente; 
+     
+
+}
+}
+
